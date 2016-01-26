d8771111371c3c7cba1801e5708af24ca51f2282
Bracken Dawson <abdawson@gmail.com>
2016-01-26 22:46:05 +0000

    Post teh temps

---
+++ mqtt_config.py
@@ -0,0 +1,22 @@
+import paho.mqtt.client
+import socket
+import sys
+
+#Please ask for an application user and password for deployment
+user_name = ""
+password = ""
+server = "spacehub.somakeit.org.uk"
+port = 1883
+
+
+#Don't normally need to edit below here
+
+#23-character clientid based on hostname and script name
+_hostname = socket.gethostname()
+_scriptname = sys.argv[0].split("/")[-1]
+while (len(_hostname) + len(_scriptname) > 22):
+    if (len(_hostname) > len(_scriptname)):
+        _hostname = _hostname[:-1]
+    else:
+        _scriptname = _scriptname[:-1]
+client_id = _hostname + "-" + _scriptname
